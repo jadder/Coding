9026522d11c72384869564d9c07a560359e7da7a
Jadder Moya <jadder.moya@gmail.com>
2025-08-25 12:33:33 +0200

    Median of Two Sorted Array

--- python/MedianofTwoSortedArrays.py
+++ python/MedianofTwoSortedArrays.py
@@ -5,5 +5,46 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
+        idx_1, idx_2 = len(nums1), len(nums2)
+        newList = []
+        exit1, exit2 = False, False
+        x, y  = 0 , 0
+        
+        while exit1 is False and exit2 is False:
+            if x < idx_1 : valueA = nums1[x] 
+            else: 
+                valueA = None
+                exit1 = True
+            
+            if y < idx_2 : valueB = nums2[y]
+            else: 
+                valueB = None
+                exit2 = True
 
-        return 0
+            if valueA is not None and valueB is not None:
+                if valueA < valueB:
+                    newList.append(valueA)
+                    x += 1
+                else:
+                    newList.append(valueB)
+                    y += 1
+            else:
+                if valueA is None:
+                    for jj in range(y, idx_2):
+                        newList.append(nums2[jj])
+                else:
+                    for ii in range(x, idx_1):
+                        newList.append(nums1[ii])
+                
+                exit1, exit2 = True, True
+
+        pos = idx_1 + idx_2
+        solution = 0
+        if pos % 2 != 0:
+            solution = newList[pos // 2]
+        else:
+            p = pos // 2
+            solution = (newList[ p-1] + newList[p])
+            solution /= float(2.0)
+
+        return solution
