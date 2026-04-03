a977d8c47b15b6a7757ae67a3abe962b0433c7d3
Jadder Moya <jadder.moya@gmail.com>
2026-04-03 10:55:26 +0200

    Updating the Sort Color challenge.

--- TypeScript/NeetCode/Array & Hashing/Group Anagrams.ts
+++ TypeScript/NeetCode/Array & Hashing/Group Anagrams.ts
@@ -1,4 +1,4 @@
-/* 
+/*
     https://neetcode.io/problems/anagram-groups/question
     Group Anagrams
     Time Complexity: O(n * k log k) -> n = number of words, k = length of the longest word
@@ -8,24 +8,23 @@ function groupAnagrams(strs: string[]): string[][] {
 
     if (strs.length === 0) { return [[""]]; } // quick return if input is empty
 
-    let sortedWords: string[] = [];
-
     // create a new array where each word is sorted alphabetically
+    let sortedWords: string[] = [];
     for (const word of strs) {
         sortedWords.push(word.split("").sort().join(""));
     }
 
-    let visited: boolean[] = new Array(strs.length); // track which words have been grouped
+    let visited: boolean[] = new Array(strs.length).fill(false); // track which words have been grouped
     let result: string[][] = new Array(); // final output array
 
     for (let r = 0; r < strs.length; r++) {
-    
-        let group: string[] = new Array(); // temporary group array
-        let key: string = ""; // to compare sorted words
-        
+
         if (visited[r]) {
             continue; // skip if already visited
         } else {
+            let group: string[] = new Array(); // temporary group array
+            let key: string = ""; // to compare sorted words
+
             for (let i = 0; i < sortedWords.length; i++) {
                 if (!visited[i]) {
                     if (group.length === 0) { // first element of this group
