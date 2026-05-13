39f476c26dcd519d3002ed3736ea15a3edbb19bb
Jadder Moya <jadder.moya@gmail.com>
2026-05-13 22:31:01 +0200

    style updated

--- c++/NeetCode/stack/CarFleet.cpp
+++ c++/NeetCode/stack/CarFleet.cpp
@@ -7,29 +7,33 @@ class Solution
 {
 
 public:
-   int carFleet(int target, vector<int>& position, vector<int>& speed) {
-    int n = position.size();
-    vector<pair<int,double>> cars;
+    int carFleet(int target, vector<int> &position, vector<int> &speed)
+    {
+        int n = position.size();
+        vector<pair<int, double>> cars;
 
-    for (int i = 0; i < n; i++) {
-        double time = (double)(target - position[i]) / speed[i];
-        cars.push_back({position[i], time});
-    }
-
-    sort(cars.begin(), cars.end(), greater<pair<int,double>>());
-
-    int fleets = 0;
-    double maxTime = 0;
-
-    for (auto &c : cars) {
-        if (c.second > maxTime) {
-            fleets++;
-            maxTime = c.second;
+        for (int i = 0; i < n; i++)
+        {
+            double time = (double)(target - position[i]) / speed[i];
+            cars.push_back({position[i], time});
         }
-    }
 
-    return fleets;
-}
+        sort(cars.begin(), cars.end(), greater<pair<int, double>>());
+
+        int fleets = 0;
+        double maxTime = 0;
+
+        for (auto &c : cars)
+        {
+            if (c.second > maxTime)
+            {
+                fleets++;
+                maxTime = c.second;
+            }
+        }
+
+        return fleets;
+    }
 };
 
 int main()
